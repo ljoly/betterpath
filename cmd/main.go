93d7a92bc4d7f62e7a66030b501d8af29f3c4cdc
Louis-marie JOLY <ljoly@e3r13p7.42.fr>
2019-03-08 20:21:10 +0100

    Rm alias

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	bp "github.com/ljoly/betterpath"
+	"github.com/ljoly/betterpath"
 )
 
 func main() {
 
-	points := make(bp.Points, 0)
+	points := make(betterpath.Points, 0)
 	points.Parse()
 	points.SortByTimestamp()
 	points.CheckPoints()
