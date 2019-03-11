614b20d720a26d6be04289fb6f267a697d8335a4
Louis-marie JOLY <ljoly@e2r9p5.42.fr>
2019-03-11 15:07:48 +0100

    Add error handling in Parse

--- parse.go
+++ parse.go
@@ -16,22 +16,34 @@ func (points *Points) Parse() error {
 		return errors.New("No file provided")
 	}
 
-	csvFile, _ := os.Open(os.Args[1])
+	csvFile, err := os.Open(os.Args[1])
+	if err != nil {
+		return errors.New("Open error")
+	}
 	reader := csv.NewReader(bufio.NewReader(csvFile))
 
 	for {
 		line, err := reader.Read()
+
 		if err == io.EOF {
 			break
 		} else if err != nil {
 			return errors.New("Read error")
 		}
+
 		a, err := strconv.ParseFloat(line[0], 64)
+		if err != nil {
+			return errors.New("Wrong data format")
+		}
 		b, err := strconv.ParseFloat(line[1], 64)
+		if err != nil {
+			return errors.New("Wrong data format")
+		}
 		c, err := strconv.ParseInt(line[2], 10, 32)
 		if err != nil {
 			return errors.New("Wrong data format")
 		}
+
 		*points = append(*points, Point{
 			x: a,
 			y: b,
